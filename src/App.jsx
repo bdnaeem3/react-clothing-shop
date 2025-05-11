7394e22a7866c22372ea5db1b75c921fd593ff4e
Naeem Ahmed <bdnaeem3@gmail.com>
2025-05-11 11:24:05 +0600

    react-redux added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from "react-router";
+
+import { store } from './redux/store';
 
 import Homepage from "./pages/Home"
 import AboutPage from "./pages/About"
@@ -6,13 +9,15 @@ import ProductsPage from "./pages/Products"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="products" element={<ProductsPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="products" element={<ProductsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
--- src/components/SmartSearch.jsx
+++ src/components/SmartSearch.jsx
@@ -1,6 +1,8 @@
 const SmartSearch = props => {
+    const { className = {}, ...rest } = props
+
     return (
-        <button className="sticky top-[20px] left-0 z-[9] px-[25px] py-[7px] bg-[#d73977] text-white rounded-[4px] rounded-tl-none rounded-bl-none w-[100px] cursor-pointer">
+        <button className={`sticky top-[20px] left-0 z-[9] px-[25px] py-[7px] bg-[#d73977] text-white rounded-[4px] rounded-tl-none rounded-bl-none w-[100px] cursor-pointer ${className}`} {...rest}>
             Smart Filter
         </button>
     )
